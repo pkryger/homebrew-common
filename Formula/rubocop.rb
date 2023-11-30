9de2415dd37b3a0aa7a3c54a74481ac6701933c7
Przemek Kryger <pkryger@gmail.com>
2023-11-30 08:46:16 +0000

    Add rubocop

---
+++ Formula/rubocop.rb
@@ -0,0 +1,22 @@
+class Rubocop < Formula
+  desc "Ruby static code analyzer (a.k.a. linter) and code formatter."
+  homepage "https://github.com/rubocop/rubocop"
+  head "https://github.com/rubocop/rubocop.git",
+    branch: "master"
+  license "MIT License"
+
+  depends_on "ruby"
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system Formula["ruby"].opt_bin/"gem", "build", "rubocop.gemspec"
+    require "#{buildpath}/lib/rubocop/version"
+    system Formula["ruby"].opt_bin/"gem", "install", "rubocop-#{RuboCop::Version::STRING}.gem"
+    bin.install libexec/"bin/rubocop"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    system bin/"rubocop", "--version"
+  end
+end
