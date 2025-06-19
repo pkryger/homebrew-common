a79d28d365f0c02a6f154dfb9c6d993da81d037a
Przemysław Kryger <pkryger@gmail.com>
2025-06-19 09:07:08 +0100

    feat(autcommit-org): Add autocommit-org

---
+++ Formula/autocommit-org.rb
@@ -0,0 +1,29 @@
+class AutocommitOrg < Formula
+  desc "Automatically Commit and push changes in directory ~/org"
+  url "file:///dev/null"
+  version "0.0.1"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  def install
+    (bin/"autocommit-org").write <<~EOS
+      #!/bin/bash
+      set -euo pipefail
+
+      cd ~/org
+      git pull
+      git add -A
+      git commit -m "Automatic push"
+      git push
+    EOS
+  end
+
+  service do
+    run [opt_bin/"autocommit-org"]
+    environment_variables PATH: std_service_path_env,
+                          TERM: "xterm"
+    log_path "/tmp/autocommit-org.sleep"
+    error_log_path "/tmp/autocommit-org.sleep"
+    run_type :interval
+    interval 3600
+  end
+end
